07fa6fec5e26da38c79a0dee01b484cf4a5e7e0b
GeorgiiIT <boshkovgeorgii@gmail.com>
2022-04-21 11:33:45 +0300

    ready

--- js/user.js
+++ js/user.js
@@ -1,13 +1,27 @@
+// let user = {
+// 	login: ``,
+// 	password: ``,
+// 	validatePassword: function(){
+		
+// 		if (this.password.length >= 6){
+// 			return `Добро пожаловать!`;
+// 		} else{
+// 			return `Пароль должен содержать не менее 6 символов и состоять только из чисел`;
+// 		}
+// 	}
+// }
+// user.password = `hello1234`;
+// console.log(user.validatePassword());
+
+
 let user = {
 	login: ``,
 	password: ``,
 	validatePassword: function(){
-		if (this.password.length >= 6){
-			return `Добро пожаловать!`;
-		} else{
-			return `Пароль должен содержать не менее 6 символов и состоять только из чисел`;
-		}
+		 if (isNaN(this.password) || !this.password || this.password.length < 6){
+			  return `Пароль должен содержать не менее 6 символов и состоять только из чисел!`
+			} else {
+			  return `Добро пожаловать`
+			}
 	}
-}
-// user.password = `hello1234`;
-// console.log(user.validatePassword());
+ }
--- js/work.js
+++ js/work.js
@@ -1,8 +1,10 @@
-do {
-	user.password =  prompt (`Enter user password`)
-if(isNaN(user.password) || !user.password || user.password.length < 6){
-	alert (user.validatePassword());
-}
 
-	} while (isNaN(user.password) || !user.password || user.password.length < 6)
+
+
+do {
+	user.password =  prompt (`Enter user password`);
+	if(isNaN(user.password) || !user.password || user.password.length < 6){
+		 alert (user.validatePassword());
+	  }
+	} while (isNaN(user.password) || !user.password || user.password.length < 6 )
 	alert (user.validatePassword());
